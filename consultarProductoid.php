64f4935feca24e74269e19f7143fc13f5280e194
IslenaCaldas <islenacaldas@gmail.com>
2026-08-20 20:26:33 -0500

    creacion de consulta de un producto por id unico

--- consultarProductoid.php
+++ consultarProductoid.php
@@ -1,9 +1,15 @@
 <?php
-header ("Content-Type: application/json");
+//se copia el codigo de consultarProducto pero se modifica
+header ("Content-Type: application/json");//respuesta en JSON
+header ("Acces-Control-Allow-Origin *"); //comparta con todos los origenes
+header ("Acces-Control-Allow-Methods: DELETE, PUT, POST, GET"); //metodos HTTP permitidos 
+header ("Acces-Control-Allow-Headers: Origin, x-Requested-With, Content-Type, Accept ");//encabezados HTTP que pueden ser usados
 
 require "conexion.php";
-
-$sql="SELECT * FROM Producto";
+//luego de adicionar a la sentencia se declara una variable $ide con el metodo GET para luego pasarla a la varibale abierta de idProducto
+$ide=$_GET[ide];
+//se le da la sentencia de buscar especificamente algo por idProducto, este se deja abierto para que reciba de otro lado
+$sql="SELECT * FROM Producto WHERE idProducto='$ide'";
 
 $respuesta= mysqli_query($con, $sql);
 
