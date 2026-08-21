b2dbb9398d3d712964ca8266653840e0301f0932
IslenaCaldas <islenacaldas@gmail.com>
2026-08-20 21:05:09 -0500

    creacion de la a pi para consulta individual

--- consultarProductoid.php
+++ consultarProductoid.php
@@ -7,21 +7,22 @@ header ("Acces-Control-Allow-Headers: Origin, x-Requested-With, Content-Type, Ac
 
 require "conexion.php";
 //luego de adicionar a la sentencia se declara una variable $ide con el metodo GET para luego pasarla a la varibale abierta de idProducto
-$ide=$_GET[ide];
+$ide=$_GET["ide"];
+//esta linea es para evitar que el usuario digite elementos que pueda dañar mi sentencia
+$ides=mysqli_real_escape_string($con, $ide);
 //se le da la sentencia de buscar especificamente algo por idProducto, este se deja abierto para que reciba de otro lado
-$sql="SELECT * FROM Producto WHERE idProducto='$ide'";
+$sql="SELECT * FROM Producto WHERE idProducto='$ides'";
 
 $respuesta= mysqli_query($con, $sql);
 
 if(!$respuesta){
     die("Error de consulta y/o conexion");
 }else{
-    
-    while ($r=mysqli_fetch_assoc($respuesta)){
-    }
+   //recuperacion de la variable por ser un solo registro 
+   $r=mysqli_fetch_assoc ($respuesta);
 }
-
-echo json_encode($resulJson); 
+//en este caso nos va a mostrar la r
+echo json_encode($r); 
 
 mysqli_close($con);
 ?>
